7d957e6ece0cab2a7d1b853ab0d0b5af5185008c
ajummer <makeudreamempty@gmail.com>
2023-10-03 14:25:59 +0300

    Save user to db

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const userServices = require("../services/userServices.js");
 
 router.get("/login", (req, res) => {
   res.render("users/loginPage");
@@ -6,6 +7,11 @@ router.get("/login", (req, res) => {
 
 router.get("/register", (req, res) => {
   res.render("users/registerPage");
+});
+
+router.post("/register", async (req, res) => {
+  const { username, password, repeatPassword } = req.body;
+  await userServices.register({username, password, repeatPassword});
 });
 
 module.exports = router;
