0e310a87d69608ef5df699c2f31ed19afed19c24
ajummer <makeudreamempty@gmail.com>
2023-10-03 20:40:47 +0300

    create jwt token and return it to controller

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,17 +1,20 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt.js");
+
+const SECRET = "ibetyoucanthackthissecretcodedamnitbruh";
 
 exports.register = (userData) => User.create(userData);
 
 exports.login = async (username, password) => {
   // Find user
- 
+
   const user = await User.findOne({ username });
 
   if (!user) {
     throw new Error("Cannot find username or password");
   }
-  
+
   // Validate password
 
   const isValid = bcrypt.compare(password, user.password);
@@ -20,5 +23,12 @@ exports.login = async (username, password) => {
     throw new Error("Cannot find username or password");
   }
 
-  return user;
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+
+  const token = jwt.sign(payload, SECRET, { expiresIn: "2d" });
+
+  return token;
 };
