fd188b00bc9d27f3ff24888f60413f8a72587076
ajummer <makeudreamempty@gmail.com>
2023-10-03 15:00:37 +0300

    create Virtual property to validate password then hash the password

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
 });
 
-userSchema.virtual("repeatPassword")
-.set(function(value){
-    if(value !== this.password){
-        throw new mongoose.Error("Passwords don't match !")
-    }
-})
+userSchema.virtual("repeatPassword").set(function (value) {
+  if (value !== this.password) {
+    throw new mongoose.Error("Passwords don't match !");
+  }
+});
+
+userSchema.pre("save", async function () {
+  const hash =  await bcrypt.hash(this.password, 10);
+  this.password = hash;
+});
 
 const User = mongoose.model("User", userSchema);
 
