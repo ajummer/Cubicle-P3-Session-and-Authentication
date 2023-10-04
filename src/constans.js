72d81c186175a65f28eb6b8fd5bd5dacd8c80065
ajummer <makeudreamempty@gmail.com>
2023-10-04 12:04:58 +0300

    add SECRET to constants js

--- src/constans.js
+++ src/constans.js
@@ -1,6 +1,7 @@
 const constants = {
   port: 3000,
   url: "mongodb://localhost:27017/cubical",
+  SECRET: "b3d5f1da16766276b43945d057376b6bd1767ad6621720bcdc9953ce5a782ca47f29e39b948a734a7f021869c0c186c2"
 };
 
 module.exports = constants;
