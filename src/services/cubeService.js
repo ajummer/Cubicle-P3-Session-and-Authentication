0eea5e478636ea481dd0cb9d9768ff3530d2d7d3
ajummer <makeudreamempty@gmail.com>
2023-10-07 15:19:49 +0300

    Add Error handlers in cubeService
    the logic behind adding err handlers in service is because i was repeating the same code in the controllers

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,5 +1,7 @@
 const Cube = require("./../models/Cube");
 
+
+
 exports.createCube = async (cubeData) => {
   const newCube = await Cube.create(cubeData);
   return newCube;
@@ -26,17 +28,21 @@ exports.getAllCubes = async (search, from, to) => {
   return filteredCubes;
 };
 
-exports.getSingleCube = (id) => {
-  const cube = Cube.findById(id).populate("accessories");
-  return cube;
+exports.getSingleCube = async (id) => {
+  try {
+    const cube = await Cube.findById(id).populate("accessories").lean();
+    return cube;
+  } catch (err) {
+    res.redirect("/404");
+    return;
+  }
 };
 
 exports.attachAccessory = async (cubeId, accessoryId) => {
-  const cube = await this.getSingleCube(cubeId);
+  const cube = await Cube.findById(cubeId).populate("accessories")
   cube.accessories.push(accessoryId);
-  return cube.save();
-};
-
+  return  cube.save()
+}
 exports.updateCube = (cubeId, cubeData) => {
   const cube = Cube.findByIdAndUpdate(cubeId, cubeData);
   return cube;
