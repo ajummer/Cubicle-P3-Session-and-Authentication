e38b32f45b804f7ad9642d9ec453234e615cd6aa
ajummer <makeudreamempty@gmail.com>
2023-10-07 13:36:25 +0300

    Validations for Accessory and Cube models

--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 
 const accessorySchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  imageUrl: String,
+  name: {
+    type: String,
+    required: [true, "Accessory name is required !"],
+    minLength: [5, "Accessory name too short ! Must be at least 5 characters long !"],
+    match: [/[A-Za-z0-9\s]+/, "Accessory name must be alphanumeric !"],
+  },
+  description: {
+    type: String,
+    required: [true, "Accessory description is required !"],
+    minLength: [20, "Accessory description too short ! Must be at least 20 characters long !"],
+    match: [/[A-Za-z0-9\s]+/, "Accessory description must be alphanumeric !"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "Accessory image is required !"],
+    match: [/[https?://[^\s]+]/, "Accessory imageUrl should start with http or https"],
+  },
 });
 
 const Accessory = mongoose.model("Accessory", accessorySchema);
--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 
 const cubeSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  imageUrl: String,
+  name: {
+    type: String,
+    required: [true, "Cube name is required !"],
+    minLength: [5, "Cube name too short ! Must be at least 5 characters long !"],
+    match: [/[A-Za-z0-9\s]+/, "Cube name must be alphanumeric !"],
+  },
+  description: {
+    type: String,
+    required: [true, "Cube description is required !"],
+    minLength: [20, "Cube description too short ! Must be at least 20 characters long !"],
+    match: [/[A-Za-z0-9\s]+/, "Cube description must be alphanumeric !"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "Cube image is required !"],
+    match: [/[https?://[^\s]+]/, "Cube imageUrl should start with http or https"],
+  },
   difficultyLevel: Number,
   accessories: [
     {
